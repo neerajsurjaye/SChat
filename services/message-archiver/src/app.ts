d71c8bc4f3b06764dc74211d234fb2d1ad715646
Neeraj Surjaye <neerajsurjaye@gmail.com>
2025-07-15 23:12:42 +0530

    feat: Update messages table structure to use foregin key ids instead of names

--- services/message-archiver/src/app.ts
+++ services/message-archiver/src/app.ts
@@ -2,7 +2,8 @@ import { Channel, ConsumeMessage } from "amqplib";
 import HandleAmqp from "./rabbitmq/handleAmqp.js";
 import logger from "./utils/logger.js";
 import MySqlClient from "./db/db.js";
-import { Pool } from "mysql2/promise";
+import { Field, FieldPacket, Pool, RowDataPacket } from "mysql2/promise";
+import { log } from "console";
 
 async function main() {
     let handleAmqp: HandleAmqp = await HandleAmqp.getInstance();
@@ -15,19 +16,28 @@ async function main() {
 
             try {
                 await mysqlConnection.execute(
-                    "CREATE TABLE IF NOT EXISTS messages(id INT AUTO_INCREMENT PRIMARY KEY, sender VARCHAR(255), receiver VARCHAR(255), message TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
+                    "CREATE TABLE IF NOT EXISTS messages(id INT AUTO_INCREMENT PRIMARY KEY, sender int, receiver int, message TEXT, created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)"
                 );
             } catch (err) {
                 logger.error("Error while trying to create table", err);
                 throw new Error(err);
             }
 
+            // Find user id
+
+            const [ids] = await mysqlConnection.execute<RowDataPacket[]>(
+                "SELECT id FROM users where username in (?, ?)",
+                [parsedMessage.from, parsedMessage.to]
+            );
+
+            logger.debug(ids);
+
             try {
                 await mysqlConnection.execute(
                     "INSERT INTO messages(sender, receiver, message, created_at) values(?,?,?,?)",
                     [
-                        parsedMessage.from,
-                        parsedMessage.to,
+                        ids[0].id,
+                        ids[1].id,
                         parsedMessage.message,
                         new Date(parsedMessage.timestamp),
                     ]
